27bdb4ebc05eb3b7616b10e32582f9ae5233b6c6
itikiryo <hi.std1706@gmail.com>
2017-09-04 18:23:34 +0900

    test

--- app/app.js
+++ app/app.js
@@ -8,5 +8,5 @@ angular.module('myApp', [
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/index'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
